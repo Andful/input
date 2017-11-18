ed67fc2c3990541b2e9d4a17a4eec7de1f4653ff
andful <buongiorno19972@gmail.com>
2017-11-18 22:15:03 +0100

    added windows part

--- keyboard/keyboard.go
+++ keyboard/keyboard.go
@@ -1,25 +1,28 @@
 package keyboard
 
 /*
+#ifdef __WIN32
+#cgo CFLAGS:-nostdlib
 #include <stdint.h>
 #include <Windows.h>
 void PressButton(WORD key,byte value) {
 	INPUT ip;
 
 	ip.type = INPUT_KEYBOARD;
-	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.wScan = 0;
 	ip.ki.time = 0;
 	ip.ki.dwExtraInfo = 0;
 
-	ip.ki.wVk = key; // virtual-key code for the "a" key
+	ip.ki.wVk = key;
 	if (value) {
-		ip.ki.dwFlags = 0; // 0 for key press
+		ip.ki.dwFlags = 0;
 	} else {
 		ip.ki.dwFlags = KEYEVENTF_KEYUP;
 	}
 	
 	SendInput(1, &ip, sizeof(INPUT));
 }
+#endif
 */
 import(
 	"C"
