f888bd8076f9de1855c1e8b53b7d5a996b1fa649
andful <buongiorno19972@gmail.com>
2017-11-19 02:58:22 +0100

    added linux mouse

--- src/mouse/mouse.go
+++ src/mouse/mouse.go
@@ -19,10 +19,43 @@ void SetPosition(int32_t x,int32_t y) {
 }
 
 #endif
+
+#ifdef __linux__
+#cgo LDFLAGS: -lX11
+
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+void Move(int32_t dx,int32_t dy) {
+    Display *displayMain = XOpenDisplay(NULL);
+
+    if(displayMain == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    XWarpPointer(displayMain, None, None, 0, 0, 0, 0, dx, dy);
+
+    XCloseDisplay(displayMain);
+}
+
+void SetPosition(int32_t x,int32_t y) {
+	Display *displayMain = XOpenDisplay(NULL);
+
+    if(displayMain == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    Window root_window = XRootWindow(displayMain, 0);
+
+    XWarpPointer(displayMain, None, root_window, 0, 0, 0, 0, x, y);
+
+    XCloseDisplay(displayMain);
+}
+#endif
 */
-import (
-"C"
-)
+import "C"
 
 func SetPosition(x int,y int) {
 	C.SetPosition(C.int32_t(x),C.int32_t(y))
