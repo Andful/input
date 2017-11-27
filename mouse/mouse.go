1943a4ac93bcd052206c2af3f8bb31f809c3df9e
andful <buongiorno19972@gmail.com>
2017-11-27 23:11:25 +0100

    added mouse click on linux

--- mouse/mouse.go
+++ mouse/mouse.go
@@ -53,6 +53,47 @@ void SetPosition(int32_t x,int32_t y) {
 
     XCloseDisplay(display);
 }
+
+void SetButton(int button,byte value){
+    Display *display = XOpenDisplay(NULL);
+
+    XEvent event;
+    
+    if(display == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&event, 0x00, sizeof(event));
+    
+    event.xbutton.button = button;
+    event.xbutton.same_screen = True;
+
+    if(value) {
+        event.type = ButtonPress;    
+        XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+        
+        event.xbutton.subwindow = event.xbutton.window;
+        
+        while(event.xbutton.subwindow)
+        {
+            event.xbutton.window = event.xbutton.subwindow;
+            
+            XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+        }
+        
+        if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Errore nell'invio dell'evento !!!\n");
+        
+    } else {
+        event.type = ButtonRelease;
+        event.xbutton.state = 0x100;
+        
+        if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Errore nell'invio dell'evento !!!\n");
+
+    }
+    
+    XCloseDisplay(display);
+}
 #endif
 */
 import "C"
@@ -63,4 +104,16 @@ func SetPosition(x int,y int) {
 
 func Move(dx int,dy int){
 	C.Move(C.int32_t(dx),C.int32_t(dy))
+}
+
+func SetButton(button int, value bool){
+    C.SetButton(button,boolToByte(value))
+}
+
+func boolToByte(value bool) C.uint8_t {
+    if(value) {
+        return 1
+    } else {
+        return 0
+    }
 }
